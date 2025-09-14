8d7ed60e74d5dcebd8eaa2580acf0cb5bff78c71
HabibaHamdyyy <hamdyhabiba952@gmail.com>
2025-09-14 18:46:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,12 +21,7 @@ app.get("/api", (req, res) => {
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://eventx-system-frontend.vercel.app",
-    "http://localhost:5000",
-    "https://eventx-system-backend.vercel.app",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "https://eventx-system-git-main-hamdyhabiba952-9998s-projects.vercel.app"
+    "https://eventx-system-frontend.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -139,4 +134,5 @@ app.listen(PORT, () => {
 
 
 export default app;
+
 
